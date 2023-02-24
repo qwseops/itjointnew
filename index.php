e65d670ecce05d0292c16283c961a977d170fbd5
qwseops <40327168+qwseops@users.noreply.github.com>
2023-02-24 12:59:11 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html><html><head>
+	 
+
+	<title>Dolorante</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- bootstrap-css -->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+	<!--// bootstrap-css -->
+	<!-- css -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+	<!--// css -->
+	<!-- font-awesome icons -->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- //font-awesome icons -->
+	<!-- font -->
+	<link href="//fonts.googleapis.com/css?family=Francois+One" rel="stylesheet" type="text/css">
+	<link href="//fonts.googleapis.com/css?family=Cinzel+Decorative:400,700,900" rel="stylesheet" type="text/css">
+	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300" rel="stylesheet" type="text/css">
+	<!-- //font -->
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/SmoothScroll.min.js"></script>
+</head>
+<body>
+<div class="bg-img">
+	<!-- banner -->
+	<div class="banner code-banner">
+		<div class="container">
+			<div class="header">
+				<div class="logo">
+					<h1><a href="index.php">Crampi</a></h1>
+				</div>
+				<div class="top-nav">
+					<nav class="navbar navbar-default">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">Menu
+						</button>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li><a href="index.php" class="active">Home</a></li>
+								<li><a href="blog.php">Articoli</a></li>
+								<li><a href="contact.php">Contatti</a></li>
+								<li><a href="policy.php">Politica sulla privacy</a></li>
+								<li><a href="terms.php">Termini e condizioni</a></li>
+								<div class="clearfix"> </div>
+							</ul>
+						</div>
+					</nav>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!-- //banner -->
+</div>
+<!-- news -->
+<div class="news">
+	<div class="container">
+
+		<div class="news-grids">
+			<div class="news-grids-heading">
+				<h3>Popolare</h3>
+			</div>
+			
+			<div class="col-md-4 news-grid">
+				<div class="agile-news-grid-info">
+					<div class="news-grid-info-img">
+						<a href="8426e154875d7d8acc7e09c12734e57f.php"><img style="height: 255px" src="images/jamie-ginsberg-msyz4yrddyq-unsplash.jpg" alt></a>
+					</div>
+					<div class="news-grid-info-bottom">
+
+						<div class="news-grid-info-bottom-text">
+							<a href="8426e154875d7d8acc7e09c12734e57f.php">Il trattamento di agopuntura: qual ...</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+
+			<div class="col-md-4 news-grid">
+				<div class="agile-news-grid-info">
+					<div class="news-grid-info-img">
+						<a href="182ce5f4eddbe563513d3c2931e10395.php"><img style="height: 255px" src="images/julien-tromeur-mzxoxmtenla-unsplash.jpg" alt></a>
+					</div>
+					<div class="news-grid-info-bottom">
+
+						<div class="news-grid-info-bottom-text">
+							<a href="182ce5f4eddbe563513d3c2931e10395.php">Qual è la cosa peggiore di bronchi...</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+
+			<div class="col-md-4 news-grid">
+				<div class="agile-news-grid-info">
+					<div class="news-grid-info-img">
+						<a href="bc380d596ea49aa28f90a6c07c4770a3.php"><img style="height: 255px" src="images/kajetan-sumila-yax2rkyt2bq-unsplash.jpg" alt></a>
+					</div>
+					<div class="news-grid-info-bottom">
+
+						<div class="news-grid-info-bottom-text">
+							<a href="bc380d596ea49aa28f90a6c07c4770a3.php">Cosa fare dopo l'intervento chirurg...</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+
+			<div class="clearfix"> </div>
+		</div>
+
+	</div>
+</div>
+<!-- //news -->
+<!-- footer -->
+
+<!-- //footer -->
+<!-- copyright -->
+<div class="copyright">
+	<div class="container">
+		<div class="w3agile-list">
+			<ul>
+
+				<li><a href="policy.php">Politica sulla privacy</a></li>
+				<li><a href="terms.php">Termini e condizioni</a></li>
+			</ul>
+		</div>
+		<div class="agileinfo">
+			<p> Copyright ©<script>document.write(new Date().getFullYear());</script>2023
+				All rights reserved</p>
+		</div>
+	</div>
+</div>
+<div class="cookie-banner">
+	<p>
+		Il sito utilizza i cookie. Essi consentono di riconoscere l'utente e di ottenere informazioni sulle tue esperienze.Continuando la navigazione nel sito, sono d'accordo con l'utilizzo dei cookie il proprietario del sito in conformità con  <a target="_blank" href="https://en.wikipedia.org/wiki/HTTP_cookie">Cookie policy</a>
+	</p>
+	<button class="close-cookie">×</button>
+</div>
+<script>
+	window.onload = function () {
+		$('.close-cookie').click(function () {
+			$('.cookie-banner').fadeOut();
+		})
+	}
+</script>
+<script>
+	let elems = document.querySelectorAll('.server-name');
+	elems.forEach((elem) => {
+		elem.innerHTML = window.location.hostname
+	})
+</script>
+
+</body></html>
